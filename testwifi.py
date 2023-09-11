45fbc32650f672104977cb4679b69d11de5e3a7a
issouf <ulysse.bruley@universite-paris-saclay.fr>
2023-09-11 21:25:19 +0200

    v2.1

---
+++ testwifi.py
@@ -0,0 +1,19 @@
+import socket
+
+# Paramètres du serveur
+HOST = "192.168.1.25"
+PORT = 8888
+
+# Créez une socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Connectez-vous à l'ESP32
+    s.connect((HOST, PORT))
+
+    while True:
+        # Envoyez des données à l'ESP32
+        message = input("Envoyer à l'ESP32 : ")
+        s.sendall(message.encode('utf-8'))
+
+        # Recevez des données de l'ESP32
+        data = s.recv(1024).decode('utf-8')
+        print(f"Reçu de l'ESP32 : {data}")
