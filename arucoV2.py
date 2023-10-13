6dcc04c262fb6e7edd4811894d9f790dc6659f54
issouuf <ulysse.bruley@universite-paris-saclay.fr>
2023-10-13 14:38:10 +0200

    v4

--- arucoV2.py
+++ arucoV2.py
@@ -5,6 +5,13 @@ import socket
 
 
 
+camera_matrix = np.array([[275.48494487, 0, 307.36023929],
+                         [0, 274.19034322, 248.29371074],
+                         [0, 0, 1]])  
+dist_coeffs = np.array([-0.32576806, 0.13293918, 0.00102543, -0.00083957, -0.02834439])
+
+
+
 
 #marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 marker_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
@@ -18,11 +25,16 @@ while True:
     ret,frame = cap.read()
     if not ret:
         break
-    gray_frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+
+    corrected_frame = cv.undistort(frame, camera_matrix, dist_coeffs)#correction de la distorsion de la caméra
+    gray_frame = cv.cvtColor(corrected_frame, cv.COLOR_BGR2GRAY)
+    # gray_frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    
     marker_corners, marker_IDs,reject = aruco.detectMarkers(gray_frame,marker_dict,parameters=param_markers)
     if marker_corners:
         for ids,corners in zip(marker_IDs,marker_corners):
-            cv.polylines(frame, [corners.astype(np.int32)],True, (0,255,255),4,cv.LINE_AA)
+            #cv.polylines(frame, [corners.astype(np.int32)],True, (0,255,255),4,cv.LINE_AA)
+            cv.polylines(corrected_frame, [corners.astype(np.int32)],True, (0,255,255),4,cv.LINE_AA)
             #print (ids)
             corners = corners.reshape(4,2)
             corners = corners.astype(int)
@@ -34,17 +46,24 @@ while True:
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
 
             # contour du tag 
-            cv.line(frame, topLeft, topRight, (0, 255, 0), 2)
-            cv.line(frame, topRight, bottomRight, (0, 255, 0), 2)
-            cv.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
-            cv.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+            cv.line(corrected_frame, topLeft, topRight, (0, 255, 0), 2)
+            cv.line(corrected_frame, topRight, bottomRight, (0, 255, 0), 2)
+            cv.line(corrected_frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv.line(corrected_frame, bottomLeft, topLeft, (0, 255, 0), 2)
 
             cx = int((topLeft[0] + bottomRight[0]) / 2.0)
             cy = int((topLeft[1] + bottomRight[1]) / 2.0)
-            cv.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
+            cv.circle(corrected_frame, (cx, cy), 4, (0, 0, 255), -1)
             
             #affichage de la détection sur l'image
-            cv.putText(frame,str(marker_IDs),(topLeft[0],topLeft[1]-15),cv.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
+            #cv.putText(frame,str(marker_IDs),(topLeft[0],topLeft[1]-15),cv.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
+
+            font = cv.FONT_HERSHEY_PLAIN
+            print(topRight, 'id = ',ids)
+            cv.putText(corrected_frame,f"id: {ids[0]}",tuple(topRight),font,1, (0,255,0),2,cv.LINE_AA)
+            
+            #i want to print the size of the marker 
+            
 
 
 
@@ -57,7 +76,7 @@ while True:
 
 
 
-    cv.imshow("frame",frame)
+    cv.imshow("frame",corrected_frame)
     stop  = cv.waitKey(1)
     if stop == ord("s"):
         break
