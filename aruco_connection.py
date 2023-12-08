be761814bcd76776643268dc35a04cdc5ef941ce
issouuf <ulysse.bruley@universite-paris-saclay.fr>
2023-12-08 16:41:26 +0100

    Fix issue with drawing frame axes for marker 42

--- aruco_connection.py
+++ aruco_connection.py
@@ -83,9 +83,10 @@ while True:
                         #rvec42, tvec42 = cv.aruco.estimatePoseSingleMarkers(marker_corners, 6, camera_matrix, dist_coeffs)
                         #Rodrigues(rvec42,tvec42)
 
-            rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(marker_corners, 0.06, camera_matrix, dist_coeffs) # OK
+            #rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(marker_corners, 0.06, camera_matrix, dist_coeffs) # OK
 
             for i in range(len(marker_IDs)):
+                rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(marker_corners, 0.06, camera_matrix, dist_coeffs) # OK
                 cv.drawFrameAxes(corrected_frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.03)
 
                 if marker_IDs[i] == 42:
