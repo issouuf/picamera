Fix marker 42 rotation and translation calculation

aruco_connection.py
@@ -89,10 +89,12 @@ while True:
             for i in range(len(marker_IDs)):
                 cv.drawFrameAxes(corrected_frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.03)
 
+                rvecs42 = rvecs[i]
+                tvecs42 = tvecs[i]
                 if marker_IDs[i] == 42:
                     rvecs42 = rvecs[i]
                     tvecs42 = tvecs[i]
-                    mat42 = cv.Rodrigues(rvecs42)
+                    mat42 = cv.Rodrigues(rvecs42,mat42)
                 else: 
                     mat = np.eye(3,3 , dtype=np.float64)
                     mat, _ = cv.Rodrigues(rvecs[i])